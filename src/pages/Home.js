942cb6ef84208cfe5480fb3154a734263e72b604
Lizeth Rico <ricogomezlauralizeth@gmail.com>
2022-06-07 18:28:21 -0500

    Home

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import Banner from '../components/banner/Banner'
+import About from '../components/about/About'
+import Footer from '../components/footer/Footer'
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <Banner/>
+        <About/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
